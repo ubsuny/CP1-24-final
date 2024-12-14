ae794b95c5e20585d1ffcbc809c29dd18a06a2f2
Priyanka_43 <PRIYANK3@BUFFALO.EDU>
2024-12-13 20:39:36 -0500

    Fixed pylint error

--- Priyanka_43/Code/test_gps_distance.py
+++ Priyanka_43/Code/test_gps_distance.py
@@ -2,7 +2,6 @@
 This module contains unit tests for the `haversine` function, which
 Calculate the distance between two points on the Earth.
 """
-import pytest
 import numpy as np
 from gps_distance import haversine
 
@@ -14,6 +13,6 @@ def test_haversine():
     # Test with known distance between two points
     lat1, lon1 = 42.95158835, -78.83160962  # Point 1
     lat2, lon2 = 42.95158835, -78.83161934  # point 2
-    expected_distance = 0.0007911  # Approx. 0.0007911 km 
+    expected_distance = 0.0007911  # Approx. 0.0007911 km
     distance = haversine(lat1, lon1, lat2, lon2)
     assert np.isclose(distance, expected_distance, rtol=1e-2)
