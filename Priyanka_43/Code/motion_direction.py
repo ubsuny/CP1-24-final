ebe22acf1945bd57237c55225c9e95be3b5978ba
Priyanka_43 <PRIYANK3@BUFFALO.EDU>
2024-12-13 20:33:03 -0500

    Fixed pylint error

--- Priyanka_43/Code/motion_direction.py
+++ Priyanka_43/Code/motion_direction.py
@@ -6,12 +6,12 @@ import numpy as np
 
 def calculate_direction(acceleration_data):
     """
-    Calculates direction of motion from x, y, z acceleration data.
+    Calculates direction of motion from x, y acceleration data.
     Returns a list of direction angles in degrees.
     """
     directions = []
     for _, row in acceleration_data.iterrows():
-        x, y, z = row['x'], row['y'], row['z']
+        x, y = row['x'], row['y'],
         direction = np.degrees(np.arctan2(y, x))
         directions.append(direction)
     return directions
