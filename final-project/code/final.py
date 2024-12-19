61a496c9d2c3aae0defb8ba65f0d8f5d3326f5a2
Muhammad Haider Abbas <157842721+haiderabbas007@users.noreply.github.com>
2024-12-18 21:23:05 -0500

    Added F to K converter

--- final-project/code/final.py
+++ final-project/code/final.py
@@ -1,1 +1,12 @@
+def fahrenheit_to_kelvin(fahrenheit):
+    """
+    Convert Fahrenheit to Kelvin.
 
+    Parameters:
+        fahrenheit (float): Temperature in Fahrenheit.
+
+    Returns:
+        float: Temperature in Kelvin.
+    """
+    kelvin = (fahrenheit - 32) * 5/9 + 273.15
+    return kelvin
