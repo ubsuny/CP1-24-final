4e7586e4eb996978e2edff25858e6128af90da03
abruns123 <aabruns123@gmail.com>
2024-12-19 05:09:14 +0000

    deleted extra files

--- abruns123/code/test_nonlinear.py
+++
@@ -1,9 +0,0 @@
-import final
-import numpy as np
-import matplotlib.pyplot as plt
-
-def test_nl():
-    x_val=np.array(np.linspace(.1,2*np.pi,101))
-    y=np.array([5*np.sin(2.5*x+.1) for x in x_val])
-    new_p=final.gauss_newton(x_val,y,[4,2.3,1], 10)[0]
-    assert np.isclose(new_p[0], 5, rtol=1e-5)
--- abruns123/code/test_parse.py
+++
@@ -1,4 +0,0 @@
-import final
-
-def test_p():
-    assert final.parse("/workspaces/CP1-24-final/abruns123/data/experiment_1.md")==28
